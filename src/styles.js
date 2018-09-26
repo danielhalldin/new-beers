125fa4ce3067e16bff6ef46dc3ee4a7a38e88035
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2018-09-26 13:47:28 +0200

    Updated styling

--- src/styles.js
+++ src/styles.js
@@ -12,13 +12,21 @@ const Flipper = styled.div`
   transition: 0.6s;
   transform-style: preserve-3d;
   position: relative;
-  transform: ${props => "rotateY(" + (props.flipped ? 180 : 0) + "deg)"};
+  transform: ${props =>
+    "rotateY(" + (props.flipped ? 180 : 0) + "deg) translateX(-15px)"};
+`;
+
+const BeersContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 `;
 
 const BeerContainer = styled.div`
   height: 310px;
   position: relative;
   vertical-align: top;
+  cursor: pointer;
 `;
 
 const Date = styled.div`
@@ -26,73 +34,77 @@ const Date = styled.div`
   color: #ddd;
   text-align: center;
   font-weight: bold;
-  border-radius: 7px 7px 0 0;
+  border-radius: 2px 2px 0 0;
   margin: -10px -10px 5px -10px;
   height: 20px;
   padding-top: 4px;
 `;
 
+const _face = `
+  backface-visibility: hidden;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 330px;
+  background-color: #ffe;
+  border-radius: 7px;
+  box-shadow: 20px 20px 15px rgba(0, 0, 0, 0.5);
+  padding: 10px;
+  border: 5px solid #333;
+`;
+const Front = styled.div`
+  ${_face} z-index: 2;
+`;
+const Back = styled.div`
+  ${_face} z-index: -1;
+  transform: rotateY(180deg);
+`;
+
 const ImageContainer = styled.div`
   text-align: center;
-  margin: 0 0 5px 0;
   padding: 10px;
   min-height: 120px;
 `;
+
 const Image = styled.img`
   height: 120px;
   max-width: 150px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
+`;
+
+const _properties = `
+  position: absolute;
+  bottom: 0;
+  padding: 10px;
 `;
 const Right = styled.div`
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  padding: 10px;
+  ${_properties} right: 0;
 `;
 const Left = styled.div`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  padding: 10px;
-`;
-const Front = styled.div`
-  z-index: 2;
-  backface-visibility: hidden;
-  position: absolute;
-  top: 0;
-  width: 100%;
-  height: 330px;
-  background-color: #ffe;
-  border-radius: 7px;
-  box-shadow: 20px 20px 15px rgba(0, 0, 0, 0.5);
-  padding: 10px;
+  ${_properties} left: 0;
 `;
 
-const Back = styled.div`
-  z-index: -1;
-  transform: rotateY(180deg);
-  backface-visibility: hidden;
-  position: absolute;
-  top: 0;
-  width: 100%;
-  height: 330px;
-  background-color: #ffe;
-  border-radius: 7px;
-  box-shadow: 20px 20px 15px rgba(0, 0, 0, 0.5);
-  padding: 10px;
-`;
-
-const Banner = styled.div`
-  margin: 0 -9px 3px -9px;
-  padding: 1px 0 3px;
-`;
-
-const Name = styled.div`
-  font-size: 22px;
+const _title = `
   font-weight: bold;
   display: block;
   text-align: center;
   line-height: 1em;
-  padding: 0 5px;
+`;
+const Name = styled.div`
+  margin: 15px -9px 3px -9px;
+  font-size: 22px;
+  ${_title};
+`;
+const Brewery = styled.div`
+  margin: 0 -9px 3px -9px;
+  font-size: 16px;
+  ${_title};
+`;
+
+const Bold = styled.div`
+  font-weight: bold;
+  display: block;
+  margin-top: 2px;
 `;
 
 const Alcohol = styled.div`
@@ -119,40 +131,57 @@ const Category = styled.div`
   overflow: hidden;
 `;
 
-const Price = styled.div`
-  font-weight: bold;
-  display: block;
-  margin-top: 2px;
-`;
-
-const ProductId = styled.div`
-  font-weight: bold;
-  display: block;
-  margin-top: 2px;
-`;
-
 const Description = styled.div`
-  height: 310px;
-  overflow-y: auto;
-  overflow-x: hidden;
+  height: 273px;
+  overflow-y: scroll;
+  border-bottom: 1px dotted black;
 `;
 
-const Cap = styled.div`
+const _cap = `
   background-image: url("./images/cap.png");
   width: 40px;
   height: 40px;
   line-height: 40px;
   position: absolute;
   top: -10px;
-  left: -10px;
   background-size: cover;
   font-weight: bold;
   text-align: center;
 `;
 
+const Cap = styled.div`
+  ${_cap} left: -10px;
+`;
+
+const UserCap = styled.div`
+  ${_cap} right: -10px;
+`;
+
+const _links = `
+  bottom: 10px;
+  position: absolute;
+  display: block;
+  background-size: cover;
+`;
+
+const Systembolaget = styled.a`
+  ${_links} background-image: url("./images/systembolaget.png");
+  right: 10px;
+  width: 50px;
+  height: 40px;
+`;
+
+const Untappd = styled.a`
+  ${_links} background-image: url("./images/untappd.png");
+  left: 10px;
+  width: 25px;
+  height: 25px;
+`;
+
 export {
   FlipContainer,
   Flipper,
+  BeersContainer,
   BeerContainer,
   Date,
   ImageContainer,
@@ -161,14 +190,16 @@ export {
   Left,
   Front,
   Back,
-  Banner,
   Name,
+  Brewery,
   Alcohol,
   Origin,
   OriginCountry,
   Category,
-  Price,
-  ProductId,
+  Bold,
   Description,
-  Cap
+  Cap,
+  UserCap,
+  Systembolaget,
+  Untappd
 };
