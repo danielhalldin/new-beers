108c74bd272e52ae48a9a2782a78e77252d109d8
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2018-10-01 21:32:18 +0200

    Updated  http-limnk-url

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -7,7 +7,7 @@ const client = untappd_access_token => {
   return new ApolloClient({
     link: ApolloLink.from([
       new HttpLink({
-        uri: "http://localhost:4444/graphql",
+        uri: "http://data-source.ddns.net/graphql",
         headers: {
           untappd_access_token
         }
