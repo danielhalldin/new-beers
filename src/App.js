84c04bb4bdfaabec900d98b19a4f38cdcfceef4f
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2018-10-04 15:20:22 +0200

    Remove cookie expiration time

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
     const untappd_access_token = cookies.get("untappd_access_token");
 
     if (token && !untappd_access_token) {
-      cookies.set("untappd_access_token", token, { expires: 30 });
+      cookies.set("untappd_access_token", token);
       window.location.href = "/";
     }
 
