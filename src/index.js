f4275a1c9782548b83d0fe5ce8bd02fd2a349721
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2018-10-04 15:37:19 +0200

    Add fastclick

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Fastclick from "react-fastclick-alt";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Fastclick>
+    <App />
+  </Fastclick>,
+  document.getElementById("root")
+);
