d6b273e6b12277e2a94e243662682c0533c0a058
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2018-11-13 07:49:57 +0100

    Status handling in mutations

--- src/components/BeerAdmin.js
+++ src/components/BeerAdmin.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Mutation } from "react-apollo";
 import { updateUntappdId, deleteBeer } from "../queries";
 import { BeerAdminWrapper, Input, Row, Cell } from "./BeerAdmin.styles";
+import { Loader } from "./Beers.styles";
 
 const BeerAdmin = ({ systembolagetArticleId }) => {
   let untappdId;
@@ -9,72 +10,108 @@ const BeerAdmin = ({ systembolagetArticleId }) => {
   return (
     <BeerAdminWrapper onClick={e => e.stopPropagation()}>
       <Mutation mutation={updateUntappdId}>
-        {(UpdateUntappdId, { data }) => (
-          <>
-            {data ? (
+        {(UpdateUntappdId, { loading, error, data }) => {
+          if (loading) {
+            return (
+              <Loader>
+                <span className="beer" role="img" aria-label="Beer">
+                  🍺
+                </span>
+                Laddar...
+              </Loader>
+            );
+          }
+
+          if (error) {
+            return <Loader>Error :(</Loader>;
+          }
+
+          if (data) {
+            return (
               <span>
                 {data.updateUntappdId === true ? "SUCCESS" : "FAILED"}
               </span>
-            ) : (
-              <form
-                onSubmit={e => {
-                  e.preventDefault();
-                  UpdateUntappdId({
-                    variables: {
-                      systembolagetArticleId: Number(systembolagetArticleId),
-                      untappdId: Number(untappdId.value)
-                    }
-                  });
-                  untappdId.value = "";
-                }}
-              >
-                <Row>
-                  <Cell>
-                    <Input
-                      placeholder="UntappdId"
-                      id="set-uid"
-                      onFocus={e => (e.target.placeholder = "")}
-                      onBlur={e => (e.target.placeholder = "UntappdId")}
-                      innerRef={node => {
-                        untappdId = node;
-                      }}
-                    />
-                  </Cell>
-                  <Cell>
-                    <button type="submit">Uppdatera</button>
-                  </Cell>
-                </Row>
-              </form>
-            )}
-          </>
-        )}
+            );
+          }
+
+          return (
+            <form
+              onSubmit={e => {
+                e.preventDefault();
+                UpdateUntappdId({
+                  variables: {
+                    systembolagetArticleId: Number(systembolagetArticleId),
+                    untappdId: Number(untappdId.value)
+                  }
+                });
+                untappdId.value = "";
+              }}
+            >
+              <Row>
+                <Cell>
+                  <Input
+                    placeholder="UntappdId"
+                    id="set-uid"
+                    onFocus={e => (e.target.placeholder = "")}
+                    onBlur={e => (e.target.placeholder = "UntappdId")}
+                    innerRef={node => {
+                      untappdId = node;
+                    }}
+                  />
+                </Cell>
+                <Cell>
+                  <button type="submit">Uppdatera</button>
+                </Cell>
+              </Row>
+            </form>
+          );
+        }}
       </Mutation>
+
       <Mutation mutation={deleteBeer}>
-        {(DeleteBeer, { data }) => (
-          <>
-            {data ? (
-              <span>{data.deleteBeer === true ? "SUCCESS" : "FAILED"}</span>
-            ) : (
-              <form
-                onSubmit={e => {
-                  e.preventDefault();
-                  DeleteBeer({
-                    variables: {
-                      systembolagetArticleId: Number(systembolagetArticleId)
-                    }
-                  });
-                }}
-              >
-                <Row>
-                  <Cell>Ta bort öl</Cell>
-                  <Cell>
-                    <button type="submit">Ta bort</button>
-                  </Cell>
-                </Row>
-              </form>
-            )}
-          </>
-        )}
+        {(DeleteBeer, { loading, error, data }) => {
+          if (loading) {
+            return (
+              <Loader>
+                <span className="beer" role="img" aria-label="Beer">
+                  🍺
+                </span>
+                Laddar...
+              </Loader>
+            );
+          }
+
+          if (error) {
+            return <Loader>Error :(</Loader>;
+          }
+
+          if (data) {
+            return (
+              <span>
+                {data.updateUntappdId === true ? "SUCCESS" : "FAILED"}
+              </span>
+            );
+          }
+          return (
+            <form
+              onSubmit={e => {
+                e.preventDefault();
+                DeleteBeer({
+                  variables: {
+                    systembolagetArticleId: Number(systembolagetArticleId)
+                  }
+                });
+              }}
+            >
+              <Row>
+                <Cell>Ta bort öl</Cell>
+                <Cell>
+                  <button type="submit">Ta bort</button>
+                </Cell>
+              </Row>
+            </form>
+          );
+        }}
       </Mutation>
     </BeerAdminWrapper>
   );
