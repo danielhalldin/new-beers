9456c7512e6ff2d4e87f6669e88683e596b1b966
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2018-11-20 15:30:06 +0100

    Lazy load components

--- src/lib/routes.js
+++ src/lib/routes.js
@@ -1,31 +1,51 @@
-import React from "react";
-
-import Beers from "../components/Beers";
-import Checkins from "../components/Checkins";
+import React, { Suspense, lazy } from "react";
+const Beers = lazy(() => import("../components/Beers"));
+const Checkins = lazy(() => import("../components/Checkins"));
+const suspenceFallback = <div>Loading...</div>;
 
 const routes = [
   {
-    componenet: <Beers stockType="Små partier" />,
+    componenet: (
+      <Suspense fallback={suspenceFallback}>
+        <Beers stockType="Små partier" />
+      </Suspense>
+    ),
     path: "/sma-partier",
     id: "Små partier"
   },
   {
-    componenet: <Beers stockType="Lokalt och småskaligt" />,
+    componenet: (
+      <Suspense fallback={suspenceFallback}>
+        <Beers stockType="Lokalt och småskaligt" />
+      </Suspense>
+    ),
     path: "/lokalt-och-smaskaligt",
     id: "Lokalt och småskaligt"
   },
   {
-    componenet: <Beers stockType="Övrigt sortiment" />,
+    componenet: (
+      <Suspense fallback={suspenceFallback}>
+        <Beers stockType="Övrigt sortiment" />
+      </Suspense>
+    ),
     path: "/ovrigt-sortiment",
     id: "Övrigt sortiment"
   },
   {
-    componenet: <Beers stockType="Ordinarie sortiment" />,
+    componenet: (
+      <Suspense fallback={suspenceFallback}>
+        <Beers stockType="Ordinarie sortiment" />
+      </Suspense>
+    ),
     path: "/ordinarie-sortiment",
     id: "Ordinarie sortiment"
   },
   {
-    componenet: <Checkins stockType="Checkins" />,
+    componenet: (
+      <Suspense fallback={suspenceFallback}>
+        <Checkins stockType="Checkins" />
+      </Suspense>
+    ),
     path: "/checkins",
     id: "Checkins"
   }
