73f7e8cf4f43cdfdff6b2c84c7ab7357e149dbfd
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2019-11-19 16:37:41 +0100

    Notification: Require interaction

--- public/worker.js
+++ public/worker.js
@@ -3,6 +3,7 @@ self.addEventListener("push", ev => {
   const data = ev.data.json();
   self.registration.showNotification(data.title, {
     body: data.body,
-    icon: data.icon
+    icon: data.icon,
+    requireInteraction: true
   });
 });
