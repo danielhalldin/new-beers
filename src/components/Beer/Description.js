bbccaa69e35d769c0b176e04786044c19e11641d
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2020-01-28 19:04:31 +0100

    Revert "try -webkit-overflow-scrolling"
    This reverts commit b60c30b59c968c89c367ea1b0046bef91fe8d837.

--- src/components/Beer/Description.js
+++ src/components/Beer/Description.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 const StyledDescription = styled.div`
   position: absolute;
   overflow-y: scroll;
-  -webkit-overflow-scrolling: touch;
   padding: 0 5px;
   height: 202px;
 `;
