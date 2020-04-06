706c69e64e7b5a2d69d690822c76fa9ba32fbab2
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2020-04-06 12:09:04 +0200

    Revert "Use video insted of gif"
    This reverts commit 5392f0941ff223a7019373069d361a74bcfb3047.

--- src/components/Beer/Image.tsx
+++ src/components/Beer/Image.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from "react";
 import styled from "styled-components/macro";
 import Img from "react-image";
 
-import beerBadgeDefaultVideo from "../../images/beer.mp4";
+import beerBadgeDefaultImage from "../../images/beer-animated.gif";
 import spinner from "../../images/spinner.svg";
 
 const ImageContainer = styled.div`
@@ -11,15 +11,6 @@ const ImageContainer = styled.div`
 `;
 
 const StyledImage = styled(Img)`
-  height: 120px;
-  max-width: 150px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
-  border: 5px solid white;
-  border-radius: 5px;
-  background-color: white;
-`;
-
-const StyledVideo = styled.video`
   height: 120px;
   max-width: 150px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
@@ -41,23 +32,13 @@ const Loader = styled.div`
 `;
 
 const Image: FunctionComponent<{ src: string }> = ({ src }) => {
+  let imageSrc = src;
   if (!src || src.includes("badge-beer-default.png")) {
-    return (
-      <ImageContainer>
-        <StyledVideo
-          autoPlay={true}
-          loop={true}
-          muted={true}
-          playsInline={true}
-        >
-          <source src={beerBadgeDefaultVideo} type="video/mp4" />
-        </StyledVideo>
-      </ImageContainer>
-    );
+    imageSrc = beerBadgeDefaultImage;
   }
   return (
     <ImageContainer>
-      <StyledImage alt="Beer" src={src} loader={<Loader />} />
+      <StyledImage alt="Beer" src={imageSrc} loader={<Loader />} />
     </ImageContainer>
   );
 };
