46e5cc8f7f0cb0418cc2c7131d0114f18c7b4111
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2020-04-16 15:18:43 +0200

    Nicer loader for beers

--- src/components/Beers/styles.ts
+++ src/components/Beers/styles.ts
@@ -2,18 +2,20 @@ import styled, { keyframes } from "styled-components/macro";
 import { colors } from "../global.styles";
 
 const Loader = styled.div`
+  display: grid;
+  align-content: center;
+  justify-content: center;
+  height: calc(100vh - 35px);
   font-family: "Bowlby One SC", cursive;
   text-shadow: 20px 20px 15px rgba(0, 0, 0, 0.5);
-  width: 100%;
+  width: 100vw;
   text-align: center;
-  padding-top: 50px;
   font-size: 24px;
   font-weight: 300;
   && .beer {
     font-size: 60px;
     display: block;
   }
-  margin-top: 30px;
 `;
 
 const FadeInAnimation = keyframes`  
