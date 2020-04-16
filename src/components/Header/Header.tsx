e52c360dedee80f5071092a6e37bafa50b088659
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2020-04-16 14:12:24 +0200

    Nicer header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,52 +4,77 @@ import { useQuery } from "@apollo/react-hooks";
 import { untappdUser } from "../../queries";
 
 import { Header, Avatar, UserName, TotalBeers, MainLink } from "./styles";
-import { Loader } from "../Beers/styles";
+import spinner from "../../images/spinner.svg";
 
 const HeaderContainer: FunctionComponent<{ login?: boolean }> = ({ login }) => {
-  return login ? (
-    <Header>
-      <MainLink href="#main">Skip to main</MainLink>
-      <h1>New Beers</h1>
-    </Header>
-  ) : (
-    <WithUser />
-  );
-};
+  let _loading, _error, _data;
+  if (!login) {
+    const { loading, error, data } = useQuery(untappdUser);
+    _loading = loading;
+    _error = error;
+    _data = data;
+  }
 
-const WithUser = () => {
-  const { loading, error, data } = useQuery(untappdUser);
-
-  if (loading)
-    return (
-      <Loader>
-        <span role="img" aria-label="Beer">
-          🍺
-        </span>{" "}
-        Laddar...
-      </Loader>
-    );
-
-  if (error) return <Loader>Error :(</Loader>;
-
-  const { avatar, name, totalBeers } = data.untappdUser;
   return (
     <Header>
       <MainLink href="#main">Skip to main</MainLink>
-      <div>
-        <Avatar alt={name} src={avatar} />
-        <UserName>{name}</UserName>
-      </div>
+      <Left login={login} loading={_loading} data={_data} error={_error} />
       <h1>New Beers</h1>
-      <div>
-        <TotalBeers>
-          {totalBeers}{" "}
-          <span role="img" aria-label="Beer">
-            🍺
-          </span>
-        </TotalBeers>
-      </div>
+      <Right login={login} loading={_loading} data={_data} error={_error} />
     </Header>
+  );
+};
+
+interface UserProps {
+  loading: any;
+  error: any;
+  data: any;
+  login: any;
+}
+
+const Left: FunctionComponent<UserProps> = ({
+  loading,
+  error,
+  data,
+  login,
+}) => {
+  if (login || error) {
+    return <div />;
+  }
+  if (loading) {
+    return <img src={spinner} />;
+  }
+  const { avatar, name } = data.untappdUser;
+  return (
+    <div>
+      <Avatar alt={name} src={avatar} />
+      <UserName>{name}</UserName>
+    </div>
+  );
+};
+
+const Right: FunctionComponent<UserProps> = ({
+  loading,
+  error,
+  data,
+  login,
+}) => {
+  if (login || error) {
+    return <div />;
+  }
+  if (loading) {
+    return <img src={spinner} />;
+  }
+  const { totalBeers } = data.untappdUser;
+  return (
+    <div>
+      <TotalBeers>
+        {totalBeers}{" "}
+        <span role="img" aria-label="Beer">
+          🍺
+        </span>
+      </TotalBeers>
+    </div>
   );
 };
 
