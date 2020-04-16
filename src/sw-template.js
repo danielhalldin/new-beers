dd077a9c187fd7f0be3b04475a3b0d13bb3f5741
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2020-04-16 15:23:23 +0200

    Back to StaleWhileRevalidate

--- src/sw-template.js
+++ src/sw-template.js
@@ -11,7 +11,7 @@ if ("function" === typeof importScripts) {
   if (workbox) {
     const { precacheAndRoute } = workbox.precaching;
     const { registerRoute } = workbox.routing;
-    const { StaleWhileRevalidate, CacheFirst } = workbox.strategies;
+    const { StaleWhileRevalidate } = workbox.strategies;
     const { ExpirationPlugin } = workbox.expiration;
 
     const cacheNames = {
@@ -37,7 +37,7 @@ if ("function" === typeof importScripts) {
     /* Dynamic cache */
     registerRoute(
       matches.images,
-      new CacheFirst({
+      new StaleWhileRevalidate({
         cacheName: cacheNames.images,
         plugins: [
           new ExpirationPlugin({
@@ -49,7 +49,7 @@ if ("function" === typeof importScripts) {
     );
     registerRoute(
       matches.graph,
-      new CacheFirst({
+      new StaleWhileRevalidate({
         cacheName: cacheNames.graphql,
         plugins: [
           new ExpirationPlugin({
