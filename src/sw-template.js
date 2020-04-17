f2cb5680117df4d6561c0205346be1fd8e2fd60b
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2020-04-17 12:10:16 +0200

    Update cache-version

--- src/sw-template.js
+++ src/sw-template.js
@@ -7,7 +7,7 @@ if ("function" === typeof importScripts) {
     "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
   );
 
-  const version = 2;
+  const version = 3;
   const customCacheNames = {
     graphql: "dynamic-graphql-" + version,
     images: "dynamic-images-" + version,
