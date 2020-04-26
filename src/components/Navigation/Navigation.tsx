90ffe067f10b5717d7a325b91f5a48ed4ab42b33
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2020-04-26 22:14:21 +0200

    Preload on mouseover

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { withRouter, RouteComponentProps } from "react-router-dom";
+import { ApolloConsumer } from "react-apollo";
 import routes from "../../lib/routes";
 import { Navigation, Button, Icon, IconText } from "./styles";
 
@@ -11,31 +12,46 @@ const MenuComponent = ({ location: { pathname } }: RouteComponentProps) => {
     });
   };
 
-  const navigationItems = routes
-    .filter((route) => route.menuIndex > -1)
-    .sort((a, b) => (a.menuIndex <= b.menuIndex ? 1 : 0))
-    .map((route) => {
-      let className = "";
-      if (pathname.includes(route.path)) {
-        className = "selected";
-      }
-      if (route.disabled) {
-        className = "disabled";
-      }
-      return (
-        <Button
-          onClick={() => scrollToTop()}
-          to={route.disabled ? pathname : route.path}
-          className={className}
-          key={route.id}
-        >
-          <Icon src={route.icon} alt={route.id} />
-          <IconText>{route.id}</IconText>
-        </Button>
-      );
-    });
+  const NavigationComponent = (client: any) => {
+    const items = routes
+      .filter((route) => route.menuIndex > -1)
+      .sort((a, b) => (a.menuIndex <= b.menuIndex ? 1 : 0))
+      .map((route) => {
+        let className = "";
+        if (pathname.includes(route.path)) {
+          className = "selected";
+        }
+        if (route.disabled) {
+          className = "disabled";
+        }
+        return (
+          <ApolloConsumer key={route.id}>
+            {(client) => (
+              <Button
+                onClick={() => scrollToTop()}
+                onMouseOver={() => {
+                  if (route.query) {
+                    client.query({
+                      query: route.query,
+                      variables: route.queryVariables,
+                    });
+                  }
+                }}
+                to={route.disabled ? pathname : route.path}
+                className={className}
+              >
+                <Icon src={route.icon} alt={route.id} />
+                <IconText>{route.id}</IconText>
+              </Button>
+            )}
+          </ApolloConsumer>
+        );
+      });
 
-  return <Navigation>{navigationItems}</Navigation>;
+    return <Navigation>{items}</Navigation>;
+  };
+
+  return <NavigationComponent />;
 };
 
 export default withRouter(MenuComponent);
