a802bc53ea2d1128863202c595971a3a882494d4
Daniel Halldin <daniel.halldin@bonnierbroadcasting.com>
2020-05-10 17:26:19 +0200

    Comment out device-orientation-code

--- src/components/Beers/Beers.tsx
+++ src/components/Beers/Beers.tsx
@@ -10,7 +10,7 @@ import { Route as RouteType } from "../../types/Route";
 import { Beer as BeerType } from "../../types/Beer";
 
 const Beers: FunctionComponent<{ path: string }> = ({ path }) => {
-  const [orient, setOrient] = useState("nothing");
+  // const [orient, setOrient] = useState("nothing");
   const currentRoute = routes.find((route: RouteType) => route.path === path);
   const query = currentRoute?.query || recommemded;
   const variables = currentRoute?.queryVariables;
@@ -56,37 +56,37 @@ const Beers: FunctionComponent<{ path: string }> = ({ path }) => {
     return <Card rotate={rotation} key={key} data={beer} admin={admin} />;
   });
 
-  const permission = () => {
-    if (
-      typeof DeviceMotionEvent !== "undefined" &&
-      typeof DeviceMotionEvent.requestPermission === "function"
-    ) {
-      // (optional) Do something before API request prompt.
-      DeviceMotionEvent.requestPermission()
-        .then((response) => {
-          // (optional) Do something after API prompt dismissed.
-          if (response == "granted") {
-            window.addEventListener("devicemotion", (e) => {
-              // do something for 'e' here.
-            });
-          }
-        })
-        .catch(console.error);
-    } else {
-      alert("DeviceMotionEvent is not defined");
-    }
-    return;
-  };
+  // const permission = () => {
+  //   if (
+  //     typeof DeviceMotionEvent !== "undefined" &&
+  //     typeof DeviceMotionEvent.requestPermission === "function"
+  //   ) {
+  //     // (optional) Do something before API request prompt.
+  //     DeviceMotionEvent.requestPermission()
+  //       .then((response) => {
+  //         // (optional) Do something after API prompt dismissed.
+  //         if (response == "granted") {
+  //           window.addEventListener("devicemotion", (e) => {
+  //             // do something for 'e' here.
+  //           });
+  //         }
+  //       })
+  //       .catch(console.error);
+  //   } else {
+  //     alert("DeviceMotionEvent is not defined");
+  //   }
+  //   return;
+  // };
 
-  window.addEventListener("deviceorientation", function (event) {
-    console.log("deviceorientation");
-    console.log(event.alpha + " : " + event.beta + " : " + event.gamma);
-    setOrient(event.alpha + " : " + event.beta + " : " + event.gamma);
-  });
+  // window.addEventListener("deviceorientation", function (event) {
+  //   console.log("deviceorientation");
+  //   console.log(event.alpha + " : " + event.beta + " : " + event.gamma);
+  //   setOrient(event.alpha + " : " + event.beta + " : " + event.gamma);
+  // });
 
   return (
     <BeersWrapper>
-      <span style={{ position: "absolute", top: "120px", left: "20px" }}>
+      {/* <span style={{ position: "absolute", top: "120px", left: "20px" }}>
         {orient}
       </span>
       <button
@@ -94,7 +94,7 @@ const Beers: FunctionComponent<{ path: string }> = ({ path }) => {
         onClick={() => permission()}
       >
         Permission
-      </button>
+      </button> */}
       <Header>{currentRoute?.id}</Header>
       <BeersContainer id={"main"}>{beers}</BeersContainer>
     </BeersWrapper>
